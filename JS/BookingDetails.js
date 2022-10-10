e4eeb1c957618f936f674bd93c01da63c41d7ae4
asit <asitjoshi20733@acropolis.in>
2022-10-11 04:36:16 +0530

    pull

--- JS/BookingDetails.js
+++ JS/BookingDetails.js
@@ -1,14 +1,14 @@
 function showFunction() {
     let num = document.getElementById("Tno").value;
     for (let i = 1; i = num; i++) {
+        const div=document.querySelector("#traveler-details");
+        const node=document.querySelector(".T-form-grp");
+        const clone=node.cloneNode(true);
+        div.appendChild(clone);
+        clone.getElementsByTagName("*")[0].innerHTML=`Traveler ${i}`;
         
         
     }
-    const div=document.querySelector("#traveler-details");
-    const node=document.querySelector(".T-form-grp");
-    const clone=node.cloneNode(true);
-    div.appendChild(clone);
-    clone.getElementsByTagName("*")[0].innerHTML=`Traveler ${i}`;
     document.querySelector("#submitBtn").style.display = 'block';
     document.querySelector("#pBtn").style.display = 'none';
 }
