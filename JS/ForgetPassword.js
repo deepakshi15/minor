c4f4e84c6ad1fa74653de18f0a9d01162c56d3fb
AsitJoshi <104497423+AsitJoshi@users.noreply.github.com>
2022-11-18 10:50:19 +0530

    Delete JS directory

--- JS/ForgetPassword.js
+++
@@ -1,5 +0,0 @@
-function show(){
-    document.querySelector("#sendBtn").style.display="none";
-    document.querySelector("#Code").style.display="block";
-    document.querySelector("#newpass").style.display="block";
-}
